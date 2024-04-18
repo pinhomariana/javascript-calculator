e4d7ffda626b1e6bcecf389cf380047175e4466d
pinhomariana <marianapinho1109@gmail.com>
2024-04-18 09:50:23 +0100

    Updated addToDisplay function"

--- javascript.js
+++ javascript.js
@@ -1,7 +1,17 @@
 const display = document.querySelector('input');
 
 function addToDisplay(input){
-    display.value += input;
+    const lastCharacter = display.value.charAt(display.value.length-1);
+
+    const operators = ["+", "-", "/", "*"];
+
+    if (operators.includes(lastCharacter) && operators.includes(input)) {
+        display.value = display.value.replace(lastCharacter, input);   
+    }
+            
+    else {
+        display.value += input;
+    }
 }
 
 function clearAll(){
